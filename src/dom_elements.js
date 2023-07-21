3ada9a4193debc6e1b46c608fcb50d8a94cfbb26
Nangamso <na.tshwete@gmail.com>
2023-07-21 13:03:11 +1000

    projects added to projects array

--- src/dom_elements.js
+++ src/dom_elements.js
@@ -3,23 +3,24 @@ const asideEl = document.createElement("div");
 const mainEl = document.createElement("div");
 const addProjectBtnEl = document.createElement("button");
 const addTodoBtnEl = document.createElement("button");
-const projectName = document.createElement("h1");
-contentEl.setAttribute("id", "content");
+const projectNameEl = document.createElement("h1");
 
 const renderSkeleton = () => {
   contentEl.setAttribute("id", "content");
   asideEl.classList.add("aside");
   addProjectBtnEl.textContent = "ADD NEW PROJECT";
+  addProjectBtnEl.classList.add("site-buttons");
   asideEl.appendChild(addProjectBtnEl);
 
-  projectName.textContent = "PROJECT NAME";
-  mainEl.appendChild(projectName);
+  projectNameEl.textContent = "PROJECT NAME";
+  mainEl.appendChild(projectNameEl);
   mainEl.classList.add("main");
   addTodoBtnEl.textContent = "ADD NEW TASK";
+  addTodoBtnEl.classList.add("site-buttons");
   mainEl.appendChild(addTodoBtnEl);
 
   contentEl.appendChild(asideEl);
   contentEl.appendChild(mainEl);
 };
 
-export { renderSkeleton, addProjectBtnEl, addTodoBtnEl };
+export { renderSkeleton, addProjectBtnEl, addTodoBtnEl, asideEl};
--- src/project.js
+++ src/project.js
@@ -1,17 +1,54 @@
-import { addProjectBtnEl } from "./dom_elements";
+import { addProjectBtnEl, asideEl } from "./dom_elements";
 let arrProjects = [];
+
+//Add project form  DOM elements
 const projectItemEl = document.createElement("div");
-const addProjectModal = document.createElement("h1");
 const contentEL = document.getElementById("content");
 const overlayEl = document.createElement("div");
 const submitProjectBtn = document.createElement("button");
+const addProjectFormEl = document.createElement("div");
+const projectTitleEl = document.createElement("label");
+const projectDescEl = document.createElement("label");
+const projectTitleInp = document.createElement("input");
+const projectDescInp = document.createElement("input");
+const cancelProjectBtn = document.createElement("button");
+const projectTitleDiv = document.createElement("div");
+const projectDescDiv = document.createElement("div");
+const projectBtnDiv = document.createElement("div");
+const projectTitle = document.createElement("h2");
 
 const renderNewProjectForm = () => {
+  asideEl.append(projectItemEl);
+  projectItemEl.classList.add("project-item-div");
+
   overlayEl.classList.add("overlay");
   contentEL.appendChild(overlayEl);
-  //form to input projects name and description
-  // sumbit button adds project to array and adds projects name to side par
-  //close over law when it clickerd
+  addProjectFormEl.setAttribute("id", "add-project-div");
+  overlayEl.appendChild(addProjectFormEl);
+
+  projectTitleDiv.classList.add("form-div");
+  addProjectFormEl.appendChild(projectTitleDiv);
+
+  projectTitleEl.textContent = "Project Name";
+  projectTitleDiv.appendChild(projectTitleEl);
+  projectTitleDiv.appendChild(projectTitleInp);
+
+  projectDescDiv.classList.add("form-div");
+  addProjectFormEl.appendChild(projectDescDiv);
+
+  projectDescEl.textContent = "Project Description";
+  projectDescDiv.appendChild(projectDescEl);
+  projectDescDiv.appendChild(projectDescInp);
+
+  projectBtnDiv.classList.add("form-div");
+  addProjectFormEl.appendChild(projectBtnDiv);
+  projectBtnDiv.appendChild(submitProjectBtn);
+  submitProjectBtn.classList.add("submitButton");
+  submitProjectBtn.textContent = "Submit";
+
+  projectBtnDiv.appendChild(cancelProjectBtn);
+  cancelProjectBtn.classList.add("cancelButton");
+  cancelProjectBtn.textContent = "Cancel";
 };
 
 const createNewProject = () => {
@@ -19,5 +56,27 @@ const createNewProject = () => {
     renderNewProjectForm();
   });
 };
+
+cancelProjectBtn.addEventListener("click", () => {
+  overlayEl.remove();
+});
+
+//Object constructor for a new project
+function Project(Name, Description) {
+  this.Name = Name;
+  this.Description = Description;
+}
+
+submitProjectBtn.addEventListener("click", () => {
+  const project = new Project(projectTitleInp.value, projectDescInp.value);
+  console.log(project);
+  arrProjects.push(project);
+  projectTitle.innerHTML += `${projectTitleInp.value} <br>`;
+  projectItemEl.append(projectTitle);
+  console.log(arrProjects);
+  overlayEl.remove();
+  projectTitleInp.value = " ";
+  projectDescInp.value = " ";
+});
 
 export { createNewProject };
